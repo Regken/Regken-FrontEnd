c3ce02fe766ada41f444e8ce408eb1d1695d2577
layintondev <layintondev@gmail.com>
2022-10-17 00:08:48 +0100

    profit or loss fixes

--- src/components/printingPage/profit-loss-report-table.js
+++ src/components/printingPage/profit-loss-report-table.js
@@ -12,70 +12,82 @@ import { formatDate } from "src/utils/helpers";
 export default function CollapsibleTable({ profitOrLossReport }) {
   return (
     <TableContainer component={Paper}>
-    <Table sx={{ minWidth: 650 }} aria-label="simple table">
-      <TableHead>
-        <TableRow>
-        <TableCell align="right">S/N</TableCell>
-          <TableCell align="right">Invoice</TableCell>
-          <TableCell align="right">Date</TableCell>
-          <TableCell align="right">Product name</TableCell>
-          <TableCell align="right">Cost Price</TableCell>
-          <TableCell align="right">Selling Price</TableCell>
-          <TableCell align="right">Quantity</TableCell>
-          <TableCell align="right">Total Selling Price</TableCell>
-          <TableCell align="right">Profit</TableCell>
-          <TableCell align="right">Loss</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {profitOrLossReport?.map((row, index) => (
-         
-          <TableRow key={row._id} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-             <TableCell align="right">{index + 1}</TableCell>
-            <TableCell align="right" component="th" scope="row">
-              {row.invoice_number}
-            </TableCell>
-            <TableCell align="right">{formatDate(row.created_at)}</TableCell>
-            <TableCell align="right">{row.product}</TableCell>
-            <TableCell align="right">{row.cost_price}</TableCell>
-            <TableCell align="right">{`₦${row.selling_price}`}</TableCell>
-            <TableCell align="right">{row.quantity}</TableCell>
-            <TableCell align="right">
-              {`₦${Number(row.quantity) * Number(row.selling_price)}`}
-            </TableCell>
-            <TableCell align="right" style={{color:"green"}}>{row.selling_price - row.cost_price<0?0:row.selling_price - row.cost_price}</TableCell>
-            <TableCell align="right" style={{color:"red"}}>{row.selling_price - row.cost_price>0?0:row.selling_price-row.cost_price}</TableCell>
-            {/* <TableCell align="right">{row.previous_product_quantity}</TableCell> */}
-            
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="right">S/N</TableCell>
+            <TableCell align="right">Invoice</TableCell>
+            <TableCell align="right">Date</TableCell>
+            <TableCell align="right">Product name</TableCell>
+            <TableCell align="right">Cost Price</TableCell>
+            <TableCell align="right">Selling Price</TableCell>
+            <TableCell align="right">Quantity</TableCell>
+            <TableCell align="right">Total Selling Price</TableCell>
+            <TableCell align="right">Profit</TableCell>
+            <TableCell align="right">Loss</TableCell>
           </TableRow>
-        ))}
-        <TableRow>
-          <TableCell></TableCell>
-          <TableCell></TableCell>
-          <TableCell></TableCell>
-          <TableCell></TableCell>
-          <TableCell></TableCell>
-          <TableCell>
-            <Typography variant="h6">Total Profit</Typography>
-          </TableCell>
-          <TableCell>
-            <Typography variant="h6" color={"green"}>{`₦${profitOrLossReport?.reduce(
-              (a, c) => a + Number(c.selling_price - c.cost_price < 0?0:c.selling_price-c.cost_price),
-              0
-            )}`}</Typography>
-          </TableCell>
-          <TableCell>
-            <Typography variant="h6">Total Loss</Typography>
-          </TableCell>
-          <TableCell>
-            <Typography variant="h6" color={{color:"red"}}>{`₦${profitOrLossReport?.reduce(
-              (a, c) => a + Number(c.selling_price - c.cost_price>0?0:c.selling_price - c.cost_price),
-              0
-            )}`}</Typography>
-          </TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
-  </TableContainer>
+        </TableHead>
+        <TableBody>
+          {profitOrLossReport?.map((row, index) => (
+            <TableRow key={row._id} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+              <TableCell align="right">{index + 1}</TableCell>
+              <TableCell align="right" component="th" scope="row">
+                {row.invoice_number}
+              </TableCell>
+              <TableCell align="right">{formatDate(row.created_at)}</TableCell>
+              <TableCell align="right">{row.product}</TableCell>
+              <TableCell align="right">{row.cost_price}</TableCell>
+              <TableCell align="right">{`₦${row.selling_price}`}</TableCell>
+              <TableCell align="right">{row.quantity}</TableCell>
+              <TableCell align="right">
+                {`₦${Number(row.quantity) * Number(row.selling_price)}`}
+              </TableCell>
+              <TableCell align="right" style={{ color: "green" }}>
+                {row.selling_price - row.cost_price < 0 ? 0 : row.selling_price - row.cost_price}
+              </TableCell>
+              <TableCell align="right" style={{ color: "red" }}>
+                {row.selling_price - row.cost_price > 0 ? 0 : row.selling_price - row.cost_price}
+              </TableCell>
+              {/* <TableCell align="right">{row.previous_product_quantity}</TableCell> */}
+            </TableRow>
+          ))}
+          <TableRow>
+            <TableCell></TableCell>
+            <TableCell></TableCell>
+            <TableCell></TableCell>
+            <TableCell></TableCell>
+            <TableCell></TableCell>
+            <TableCell>
+              <Typography variant="h6">Total Profit</Typography>
+            </TableCell>
+            <TableCell>
+              <Typography variant="h6" color={"green"}>{`₦${profitOrLossReport?.reduce(
+                (a, c) =>
+                  a +
+                  Number(
+                    (c.selling_price - c.cost_price < 0 ? 0 : c.selling_price - c.cost_price) *
+                      c.quantity
+                  ),
+                0
+              )}`}</Typography>
+            </TableCell>
+            <TableCell>
+              <Typography variant="h6">Total Loss</Typography>
+            </TableCell>
+            <TableCell>
+              <Typography variant="h6" color={{ color: "red" }}>{`₦${profitOrLossReport?.reduce(
+                (a, c) =>
+                  a +
+                  Number(
+                    (c.selling_price - c.cost_price > 0 ? 0 : c.selling_price - c.cost_price) *
+                      c.quantity
+                  ),
+                0
+              )}`}</Typography>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
